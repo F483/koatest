ece8bef61af1aa79ddae91cabd1dcc388641bda5
Fabian Barkhau <f483@protonmail.com>
2024-07-05 15:40:05 +0200

    move test to module

--- api/math/routes.test.js
+++ api/math/routes.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../index');
+const app = require('../../index');
 
 describe('POST /add', () => {
     it('should add two numbers', async () => {
